2c5b8ad9e7da7395d4054c6609a3d7fadce3f648
tommyallen <thomasclansi228@gmail.com>
2024-10-27 00:23:23 +0700

    Create new Activity and add logical part for objects on first Activity

---
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.lab5
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    public lateinit var checkbox1: CheckBox
+    public lateinit var checkbox2: CheckBox
+    public lateinit var checkbox3: CheckBox
+    public lateinit var count: EditText
+    public lateinit var button: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        checkbox1 = findViewById(R.id.checkBox)
+        checkbox2 = findViewById(R.id.checkBox2)
+        checkbox3 = findViewById(R.id.checkBox3)
+        count = findViewById(R.id.editTextNumberDecimal)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener(){
+
+
+        }
+
+
+    }
+
+
+
+}
